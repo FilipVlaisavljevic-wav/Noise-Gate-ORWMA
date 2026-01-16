7f023c721fb4baa9249d35d0f8a1ea25b2362540
FilipVlaisavljevic-wav <filipvlaisavljevic59@gmail.com>
2026-01-16 21:47:26 +0100

    model.js fixes

--- scripts/model.js
+++ scripts/model.js
@@ -4,13 +4,13 @@ import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
 const scene = new THREE.Scene();
 
-const lopta = new THREE.SphereGeometry(0.2, 32, 32);
+/*const lopta = new THREE.SphereGeometry(0.2, 32, 32);
 const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
-const visualiser = new THREE.Mesh(lopta, material);
+const visualiser = new THREE.Mesh(lopta, material);*/
 
 const container = document.getElementById('turntable-container');
 const camera = new THREE.PerspectiveCamera(60, container.clientWidth/container.clientHeight, 3, 1000);
-camera.position.set(12, 35, 67);
+camera.position.set(12, 60, 67);
 if(window.innerWidth < 750){
     camera.position.set(20, 55, 100);
 }
@@ -19,7 +19,6 @@ camera.lookAt(0, 0, 0);
 const render = new THREE.WebGLRenderer({
     alpha: true, 
     antialias: true,
-    powerPreference: "high-performance"
 });
 
 
@@ -38,6 +37,7 @@ container.appendChild(render.domElement);
 
 const directLighting = new THREE.DirectionalLight(0xffffff, 1.5);
 directLighting.position.set(10, 50, 30);
+//directLighting.add(visualiser)
 directLighting.castShadow = true;
 
 directLighting.shadow.mapSize.width = 2048;
@@ -67,6 +67,11 @@ let needleArm;
 let needlePin;
 let ringsArm;
 window.addEventListener('click', onMouseClick);
+
+const mouseControl = new OrbitControls(camera, render.domElement);
+mouseControl.enableDamping = true;
+mouseControl.dampingFactor = 0.05;
+mouseControl.enableZoom = true;
 
 function onMouseClick(event){
     const rect = render.domElement.getBoundingClientRect();
@@ -142,7 +147,14 @@ function animation() {
     const start = -0.30;
     const end = -0.65;
     let targetRotation = 0;
-    const targetLower = song.paused ? 0 : 0.30;
+    let targetLower;
+    if(song.paused){
+        targetLower = 0
+    }
+    else{
+        targetLower = 0.30
+    }
+    
     
     if(song.paused === false && song.duration > 0){
         const progress = song.currentTime / song.duration;
@@ -153,7 +165,7 @@ function animation() {
         armPivot.rotation.y = THREE.MathUtils.lerp(armPivot.rotation.y, targetRotation, 0.05);
         armPivot.rotation.z = THREE.MathUtils.lerp(armPivot.rotation.z, targetLower, 0.05);
     }
-    
+    mouseControl.update();
     render.render(scene, camera);
 }
 animation();
